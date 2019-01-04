218495be1dcbceb81b62b4dda44ed382fe481011
Naldson Chagas <naldson@protonmail.com>
2019-01-04 00:07:17 -0300

    index router

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const app = require('express')();
 const movieRouter = require('./routes/movies');
+const indexRouter = require('./routes/index');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/', indexRouter);
 app.use('/movies', movieRouter);
 
 app.listen(3000, 'localhost', () => {
